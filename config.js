6bc99bea4d7626ce598c6e1873e6ff8ed081ba55
Lukasz Borek <borek_lukasz@outlook.com>
2018-08-12 15:40:45 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '960635762070-7el547is6lrnve0kl7tdmq6qcjoj4mri.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '4W-UCVqZLfh90ipum4v9kuSc',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+
+/*
+Twój identyfikator klienta:
+960635762070-7el547is6lrnve0kl7tdmq6qcjoj4mri.apps.googleusercontent.com
+
+Twój tajny klucz klienta
+4W-UCVqZLfh90ipum4v9kuSc
+*/
